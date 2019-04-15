43106df32898d96a4b512518c7a3fe992f2a7ebd
Jonathan Silverstein <jsilve@umich.ed>
2019-04-15 14:53:58 -0400

    Pretty good on the publication pages

---
+++ research.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+	<?php
+		include("head.html");
+	?>
+	<body>
+
+		<div class="site-content">
+			<?php include("generic/header.html"); ?>
+				<h1 style="text-align:center">Research</h1>
+					<div class="container">
+						<div class="row">
+							<div class="col-md-3 col-sm-6">
+								<div class="feature">
+									<img src="images/Jetting.jpg" alt="" class="feature-image">
+									<h2 class="feature-title">Vapor-Based Reactive Polymeric Coatings</h2>
+									<p>This page describes Vapor-Based Reactive Polymeric Coatings</p>
+									<a href="Electrohydrodynamic.php" class="button">Learn more</a>
+								</div>
+							</div>
+							<div class="col-md-3 col-sm-6">
+								<div class="feature">
+									<img src="images/Vapor.jpg" alt="" class="feature-image">
+									<h2 class="feature-title">Functional Molecule Synthesis</h2>
+									<p>This page describes Functional Molecule Synthesis</p>
+									<a href="Vapor.php" class="button">Learn more</a>
+								</div>
+							</div>
+							<div class="col-md-3 col-sm-6">
+								<div class="feature">
+									<img src="images/Polymer.jpg" alt="" class="feature-image">
+									<h2 class="feature-title">Electro hydrodynamic Co-Jetting of Particles and Fibers</h2>
+									<p>This page describes ElectrohyElectrohydrodynamic Co-Jetting of particles and Fibers</p>
+									<a href="Polymer.php" class="button">Learn more</a>
+								</div>
+							</div>
+							<div class="col-md-3 col-sm-6">
+								<div class="feature">
+									<img src="images/Functional.png" alt="" class="feature-image">
+									<h2 class="feature-title">Polymer Brushes for Biomedical Applications</h2>
+									<p>This page describes Polymer Brushes for Biomedical Applications</p>
+								</div>
+							</div>
+						</div> <!-- .row -->
+					</div> <!-- .container -->
+				</div> <!-- .fullwidth-block -->
+			<?php
+				include("generic/footer.html");
+			?>
+
+		<script src="js/jquery-1.11.1.min.js"></script>
+		<script src="js/plugins.js"></script>
+		<script src="js/app.js"></script>
+
+	</body>
+
+</html>
